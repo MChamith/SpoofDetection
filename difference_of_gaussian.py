409da26a8e5eb976cb8334587b5256302dccd787
MChamith <mawelacm@gmail.com>
2019-10-30 12:53:58 +0530

    dataset preperation

--- difference_of_gaussian.py
+++ difference_of_gaussian.py
@@ -14,32 +14,32 @@ def calc_dog(img):
     # print('result size ' + str(result.shape) + 'result type ' + str(type(result)))
     return result
 
-matplotlib.use('TkAgg')
-filename = 'videos/spoofing/003/003-1-3-2-2.mov'
-# original_image = img_as_float(Image.open())
-# img = color.rgb2gray(original_image)
-f = open(filename.split('.mov')[0] + '.face')
-cap = cv2.VideoCapture(filename)
-count = 0
-while True:
-    ret, frame = cap.read()
-    read_line = f.readline()
-    print(read_line)
-    if read_line == '':
-        break_the_loop = True
-    else:
-        print(read_line)
-        try:
-            x, y, w, h = map(int, read_line.strip().split(' '))
-        except:
-            print('exception')
-            pass
-        print('x= ' + str(x) + ' y= ' + str(y) + ' w= ' + str(w) + ' h= ' + str(h))
-        if w and h:
-            img = frame[y:h, x:w]
-            # img = cv2.imread('face (4).jpg')
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # print(img_arry.shape)
-            result = calc_dog(img)
-            cv2.imwrite('images/spoof/dog_new/result'+str(count)+'.jpg', result)
-            count += 1
+# matplotlib.use('TkAgg')
+# filename = 'videos/spoofing/003/003-1-3-2-2.mov'
+# # original_image = img_as_float(Image.open())
+# # img = color.rgb2gray(original_image)
+# f = open(filename.split('.mov')[0] + '.face')
+# cap = cv2.VideoCapture(filename)
+# count = 0
+# while True:
+#     ret, frame = cap.read()
+#     read_line = f.readline()
+#     print(read_line)
+#     if read_line == '':
+#         break_the_loop = True
+#     else:
+#         print(read_line)
+#         try:
+#             x, y, w, h = map(int, read_line.strip().split(' '))
+#         except:
+#             print('exception')
+#             pass
+#         print('x= ' + str(x) + ' y= ' + str(y) + ' w= ' + str(w) + ' h= ' + str(h))
+#         if w and h:
+#             img = frame[y:h, x:w]
+#             # img = cv2.imread('face (4).jpg')
+#             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#             # print(img_arry.shape)
+#             result = calc_dog(img)
+#             cv2.imwrite('images/spoof/dog_new/result'+str(count)+'.jpg', result)
+#             count += 1
--- train_model.py
+++ train_model.py
@@ -1,3 +1,34 @@
-import DataGenerator
-from SpoofModel import cnn_model
+import fnmatch
 
+# import DataGenerator
+# from SpoofModel import cnn_model
+import json
+import os
+import tqdm
+
+data = {'train': []}
+labels = {}
+
+TRAIN_DIR = 'home/ec2-user/dataset/SIWFaces/SiW_release/Train/'
+
+for root, dirnames, filenames in tqdm(os.walk(TRAIN_DIR)):
+    for filename in fnmatch.filter(filenames, "*.jpg"):
+        path = os.path.join(root, filename)
+        data['train'].append(path)
+        if path.split('/')[-3] == 'live':
+            labels[path] = 1
+        elif path.split('/') == 'spoof':
+            labels[path] = 0
+
+#       img = image.load_img(path, target_size=(128,128), grayscale = True)
+#       img_array = image.img_to_array(img)
+#       train_data.append(np.array(img_array))
+
+#   np.save('training_data.npy' , train_data)
+
+# path = 'home/ec2-user/dataset/SIWFaces/SiW_release/Train/live/003/1231.jpg'
+# print(path.split('/')[-3])
+with open('data.txt', 'w') as file:
+    file.write(json.dumps(data))
+with open('labels.txt', 'w') as file:
+    file.write(json.dumps(labels))
