554eda791963a361b6c5a964fdd0c052ff7f4f8e
MChamith <mawelacm@gmail.com>
2019-11-01 10:59:33 +0530

    added training code
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- train_model.py
+++ train_model.py
@@ -1,22 +1,30 @@
 import fnmatch
 
+import keras
+import tf as tf
+
 from DataGenerator import DataGenerator
 from SpoofModel import cnn_model
 import json
 import os
 import tqdm
 import numpy as np
+from sklearn.utils import shuffle
+from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
-data = {'train': [], 'label': []}
+data = {'X_train': [], 'label': []}
+test_data = {'X_val': [], 'val_label': [], 'X_test': [], 'test_label': []}
 # labels = {'spoof':[]}
 
 TRAIN_DIR = '/home/ec2-user/dataset/SIWFaces/SiW_release/Train/'
+TEST_DIR = '/home/ec2-user/dataset/SIWFaces/SiW_release/Test/'
 
+# create training data in
 for root, dirnames, filenames in os.walk(TRAIN_DIR):
     for filename in fnmatch.filter(filenames, "*.jpg"):
         path = os.path.join(root, filename)
         # print(path)
-        data['train'].append(path)
+        data['X_train'].append(path)
         if path.split('/')[-3] == 'live':
             data['label'].append(1)
             print('path ' + str(path) + ' label 1')
@@ -24,18 +32,53 @@ for root, dirnames, filenames in os.walk(TRAIN_DIR):
             data['label'].append(0)
             print('path ' + str(path) + ' label 0')
 
-params = {'dim': (256,256),
+count = 0
+for root, dirnames, filenames in os.walk(TEST_DIR):
+    for filename in fnmatch.filter(filenames, "*.jpg"):
+        path = os.path.join(root, filename)
+        if count % 2 == 0:
+            test_data['X_val'].append(path)
+            if path.split('/')[-3] == 'live':
+                test_data['val_label'].append(1)
+            elif path.split('/')[-3] == 'spoof':
+                test_data['val_label'].append(0)
+        else:
+            test_data['X_test'].append(path)
+            if path.split('/')[-3] == 'live':
+                test_data['test_label'].append(1)
+            elif path.split('/')[-3] == 'spoof':
+                test_data['test_label'].append(0)
+
+data['X_train'], data['label'] = shuffle(data['X_train'], data['label'])
+test_data['X_test'], test_data['test_label'] = shuffle(test_data['X_test'], test_data['test_label'])
+test_data['X_val'], test_data['val_label'] = shuffle(test_data['X_val'], test_data['val_label'])
+
+params = {'dim': (256, 256),
           'batch_size': 32,
           'n_channels': 3,
           'shuffle': False}
 
-train_gen = DataGenerator(**params, list_IDs=data['train'], labels=data['label'])
-
+train_gen = DataGenerator(**params, list_IDs=data['X_train'], labels=data['label'])
 for i in train_gen:
     print(i)
-# path = 'home/ec2-user/dataset/SIWFaces/SiW_release/Train/live/003/1231.jpg'
-# print(path.split('/')[-3])
-# with open('data.txt', 'w') as file:
-#     file.write(json.dumps(data))
-# with open('labels.txt', 'w') as file:
-#     file.write(json.dumps(labels))
+val_generator = DataGenerator(**params, list_IDs=test_data['X_val'], labels=test_data['val_label'])
+test_generator = DataGenerator(**params, list_IDs=test_data['X_test'], labels=test_data['test_label'])
+
+model = cnn_model()
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+file_path = 'Checkpoint'
+check_pointer = ModelCheckpoint(filepath=file_path)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                              patience=2, min_lr=0.00001)
+early_stop = EarlyStopping(patience=3)
+tensorboard_keras = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,
+                                                write_graph=True, write_images=True)
+
+model_history = model.fit_generator(generator=train_gen,
+                                    epochs=100,
+                                    validation_data=val_generator,
+                                    callbacks=[check_pointer,
+                                               reduce_lr, tensorboard_keras, early_stop],
+                                    shuffle=False
+                                    )
