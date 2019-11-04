4ddcc117d675ee54cdbed247896899bcb63371ce
MChamith <mawelacm@gmail.com>
2019-11-04 15:18:26 +0530

    Signed-off-by: MChamith <mawelacm@gmail.com>

--- train_model.py
+++ train_model.py
@@ -77,5 +77,5 @@ model_history = model.fit_generator(generator=train_gen,
                                     callbacks=[check_pointer,
                                                reduce_lr, tensorboard_keras, early_stop],
                                     shuffle=True,
-                                    use_multiprocessing=True, steps_per_epoch=10
+                                    use_multiprocessing=True, steps_per_epoch=1000
                                     )
