69f36f976a67801e43c7165d1f51659a681b7f0d
MChamith <mawelacm@gmail.com>
2019-11-07 21:11:12 +0530

    test model changes
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ from keras.callbacks import Callback, ModelCheckpoint, ReduceLROnPlateau, EarlyS
 
 test_data = {'X_test': [], 'label': []}
 
-TEST_DIR = '/data/SiW_release/Test/'
+TEST_DIR = 'TestData/'
 
 print('collecting training data')
 for root, dirnames, filenames in os.walk(TEST_DIR):
@@ -32,4 +32,5 @@ params = {'dim': (256, 256),
 test_gen = DataGenerator(**params, list_IDs=test_data['X_test'], labels=test_data['label'])
 
 model = keras.models.load_model('Models/Model-03.h5')
-model.evaluate_generator(generator=test_gen)
+metrics = model.evaluate_generator(generator=test_gen)
+print('test loss, test accuracy ' + str(metrics))
