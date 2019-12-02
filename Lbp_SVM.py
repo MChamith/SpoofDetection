2566b70e4835d9c3bef9d395b23969c698419914
MChamith <mawelacm@gmail.com>
2019-12-02 12:32:58 +0530

    svm apporach
    Signed-off-by: MChamith <mawelacm@gmail.com>

---
+++ Lbp_SVM.py
@@ -0,0 +1,31 @@
+import fnmatch
+import pickle
+import cv2
+import numpy as np
+
+from LocalBinaryPatterns import LocalBinaryPatterns
+import os
+
+TRAIN_DIR = '/home/ubuntu/volume/SiW_release/Train/'
+TEST_DIR = '/home/ubuntu/volume/SiW_release/Test/'
+train_data = []
+label = []
+desc = LocalBinaryPatterns(24, 8)
+for root, dirnames, filenames in os.walk(TRAIN_DIR):
+    for filename in fnmatch.filter(filenames, "*.jpg"):
+        path = os.path.join(root, filename)
+        # print(path)
+        img = cv2.imread(path)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.resize(img_gray, (256, 256))
+        img_gray = np.expand_dims(img_gray, axis=-1)
+        hist = desc.describe(img_gray)
+        train_data.append(hist)
+        if path.split('/')[-3] == 'live':
+            label.append(1)
+        elif path.split('/')[-3] == 'spoof':
+            label.append(0)
+
+with open('train_data.pickle', 'wb') as handle:
+    pickle.dump(train_data, handle)
+    pickle.dump(label, handle)
