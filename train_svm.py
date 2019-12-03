b09f7d1117c1190c6b4169f7201cd78bf93a1f64
MChamith <mawelacm@gmail.com>
2019-12-03 11:25:42 +0530

    add print statements
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- train_svm.py
+++ train_svm.py
@@ -3,9 +3,12 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 
+print('collecting train data')
+
 with open("train_data.pickle", "rb") as myfile:
     X_train = pickle.load(myfile)
     X_label = pickle.load(myfile)
+print('train data collected')
 
 with open('test_data.pickle', 'rb') as handle:
     X_test = pickle.load(handle)
@@ -14,13 +17,17 @@ with open('test_data.pickle', 'rb') as handle:
     val_label = pickle.load(handle)
 scaler = StandardScaler()
 # Fit on training set only.
+print('Scaling')
 scaler.fit(X_train)
 # Apply transform to both the training set and the test set.
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 X_val = scaler.transform(X_val)
+print('Scale transformed')
 
 model = LinearSVC(C=100.0, random_state=42)
+print('starting to fit model')
 model.fit(X_train, X_label)
+print('model fitted')
 score = model.score(X_val, val_label)
 print('score = ' + str(score))
