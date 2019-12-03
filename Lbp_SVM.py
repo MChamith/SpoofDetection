c8cd9870f5423b809e14dc67dcbb363f8dca16a4
MChamith <mawelacm@gmail.com>
2019-12-03 12:00:57 +0530

    fic
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- Lbp_SVM.py
+++ Lbp_SVM.py
@@ -15,21 +15,20 @@ print('collecting data')
 count = 0
 for root, dirnames, filenames in os.walk(TRAIN_DIR):
     for filename in fnmatch.filter(filenames, "*.jpg"):
-        if count % 40 == 0:
-            print(filename)
-            path = os.path.join(root, filename)
-            # print(path)
-            img = cv2.imread(path)
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img_gray = cv2.resize(img_gray, (256, 256))
-            hist = desc.describe(img_gray)
-            train_data.append(hist)
-            if path.split('/')[-3] == 'live':
-                label.append(1)
-            elif path.split('/')[-3] == 'spoof':
-                label.append(0)
+        print(filename)
+        path = os.path.join(root, filename)
+        # print(path)
+        img = cv2.imread(path)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.resize(img_gray, (256, 256))
+        hist = desc.describe(img_gray)
+        train_data.append(hist)
+        if path.split('/')[-3] == 'live':
+            label.append(1)
+        elif path.split('/')[-3] == 'spoof':
+            label.append(0)
         count +=1
-        print(label)
+
 
 print('dumping pickle')
 with open('train_data.pickle2', 'wb') as handle:
