6d917c70f9e4f3b3d3a4a3975e44d3099e90223c
MChamith <mawelacm@gmail.com>
2019-12-16 15:05:55 +0530

    make text path files
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- train_model.py
+++ train_model.py
@@ -23,12 +23,18 @@ print('collecting training data')
 for root, dirnames, filenames in os.walk(TRAIN_DIR):
     for filename in fnmatch.filter(filenames, "*.jpg"):
         path = os.path.join(root, filename)
+        with open('/home/ubuntu/volume/SiW_release/train_list.txt', 'a') as fw:
+            fw.write(path + ' ' + path + ' ' + path + ' ')
         # print(path)
         data['X_train'].append(path)
         if path.split('/')[-3] == 'live':
             data['label'].append(1)
+            with open('/home/ubuntu/volume/SiW_release/train_list.txt', 'a') as fw:
+                fw.write('1\n')
         elif path.split('/')[-3] == 'spoof':
             data['label'].append(0)
+            with open('/home/ubuntu/volume/SiW_release/train_list.txt', 'a') as fw:
+                fw.write('0\n')
 
 count = 0
 print('collecting validation and test data')
@@ -36,55 +42,67 @@ for root, dirnames, filenames in os.walk(TEST_DIR):
     for filename in fnmatch.filter(filenames, "*.jpg"):
         path = os.path.join(root, filename)
         if count % 10 == 0:
+            with open('/home/ubuntu/volume/SiW_release/val_list.txt', 'a') as fw:
+                fw.write(path + ' ' + path + ' ' + path + ' ')
             test_data['X_val'].append(path)
             if path.split('/')[-3] == 'live':
+                with open('/home/ubuntu/volume/SiW_release/val_list.txt', 'a') as fw:
+                    fw.write('1\n')
                 test_data['val_label'].append(1)
             elif path.split('/')[-3] == 'spoof':
+                with open('/home/ubuntu/volume/SiW_release/val_list.txt', 'a') as fw:
+                    fw.write('0\n')
                 test_data['val_label'].append(0)
         else:
+            with open('/home/ubuntu/volume/SiW_release/test_list.txt', 'a') as fw:
+                fw.write(path + ' ' + path + ' ' + path + ' ')
             test_data['X_test'].append(path)
             if path.split('/')[-3] == 'live':
+                with open('/home/ubuntu/volume/SiW_release/test_list.txt', 'a') as fw:
+                    fw.write('1\n')
                 test_data['test_label'].append(1)
             elif path.split('/')[-3] == 'spoof':
+                with open('/home/ubuntu/volume/SiW_release/test_list.txt', 'a') as fw:
+                    fw.write('0\n')
                 test_data['test_label'].append(0)
         count += 1
-print('shuffling data')
-data['X_train'], data['label'] = shuffle(data['X_train'], data['label'])
-test_data['X_test'], test_data['test_label'] = shuffle(test_data['X_test'], test_data['test_label'])
-test_data['X_val'], test_data['val_label'] = shuffle(test_data['X_val'], test_data['val_label'])
-print('data shuffled')
-params = {'dim': (128, 128),
-          'batch_size': 32,
-          'n_channels': 3,
-          'shuffle': True}
-
-val_params = {'dim': (128, 128),
-              'batch_size': 16,
-              'n_channels': 3,
-              'shuffle': False}
-
-train_gen = DataGenerator(**params, list_IDs=data['X_train'], labels=data['label'])
-val_generator = DataGenerator(**params, list_IDs=test_data['X_val'], labels=test_data['val_label'])
-test_generator = DataGenerator(**params, list_IDs=test_data['X_test'], labels=test_data['test_label'])
-
-model = cnn_model()
-print('compiling model')
-sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
-print('model compiled')
-file_path = 'Checkpoint/Model_128_input/Model-{epoch:02d}.h5'
-check_pointer = ModelCheckpoint(filepath=file_path)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
-                              patience=1, min_lr=0.00001)
-early_stop = EarlyStopping(patience=4)
-tensorboard_keras = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,
-                                                write_graph=True, write_images=True)
-
-model_history = model.fit_generator(generator=train_gen,
-                                    epochs=100,
-                                    validation_data=val_generator,
-                                    callbacks=[check_pointer,
-                                               reduce_lr, tensorboard_keras, early_stop],
-                                    shuffle=True,
-                                    steps_per_epoch=1000, validation_steps=50
-                                    )
+# print('shuffling data')
+# data['X_train'], data['label'] = shuffle(data['X_train'], data['label'])
+# test_data['X_test'], test_data['test_label'] = shuffle(test_data['X_test'], test_data['test_label'])
+# test_data['X_val'], test_data['val_label'] = shuffle(test_data['X_val'], test_data['val_label'])
+# print('data shuffled')
+# params = {'dim': (128, 128),
+#           'batch_size': 32,
+#           'n_channels': 3,
+#           'shuffle': True}
+#
+# val_params = {'dim': (128, 128),
+#               'batch_size': 16,
+#               'n_channels': 3,
+#               'shuffle': False}
+#
+# train_gen = DataGenerator(**params, list_IDs=data['X_train'], labels=data['label'])
+# val_generator = DataGenerator(**params, list_IDs=test_data['X_val'], labels=test_data['val_label'])
+# test_generator = DataGenerator(**params, list_IDs=test_data['X_test'], labels=test_data['test_label'])
+#
+# model = cnn_model()
+# print('compiling model')
+# sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+# model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy'])
+# print('model compiled')
+# file_path = 'Checkpoint/Model_128_input/Model-{epoch:02d}.h5'
+# check_pointer = ModelCheckpoint(filepath=file_path)
+# reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+#                               patience=1, min_lr=0.00001)
+# early_stop = EarlyStopping(patience=4)
+# tensorboard_keras = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,
+#                                                 write_graph=True, write_images=True)
+#
+# model_history = model.fit_generator(generator=train_gen,
+#                                     epochs=100,
+#                                     validation_data=val_generator,
+#                                     callbacks=[check_pointer,
+#                                                reduce_lr, tensorboard_keras, early_stop],
+#                                     shuffle=True,
+#                                     steps_per_epoch=1000, validation_steps=50
+#                                     )
