c9bbc2047097dfbaef99aee1cb989396f3b66c61
MChamith <mawelacm@gmail.com>
2020-01-20 16:59:00 +0530

    initial model
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- Spoof_vgg16.py
+++ Spoof_vgg16.py
@@ -1,4 +1,3 @@
-from keras.applications.vgg19 import VGG19
 from keras.preprocessing import image
 from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
@@ -8,18 +7,34 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Convolution2D, MaxPool2D, Input, Concatenate, Flatten
 import numpy as np
 import keras.backend as K
+from keras_applications.vgg19 import VGG19
+
 
 def vgg16_feature_fusion():
-    base_model = VGG19(weights='imagenet')
-    print(base_model.summary())
-    # model = Model(inputs=base_model.input, outputs=base_model.get_layer('block4_pool').output)
-    #
-    # img_path = 'elephant.jpg'
-    # img = image.load_img(img_path, target_size=(224, 224))
-    # x = image.img_to_array(img)
-    # x = np.expand_dims(x, axis=0)
-    # x = preprocess_input(x)
-    #
-    # block4_pool_features = model.predict(x
+    input_img1 = Input(shape=(224, 224, 3))  # channel first
+    input_img2 = Input(shape=(224, 224, 3))
+    input_img3 = Input(shape=(224, 224, 3))
 
-vgg16_feature_fusion()
+    base_model = VGG19(weights='imagenet')
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer('block4_pool').output)
+
+    X1 = model.predict(input_img1)
+    X2 = model.predict(input_img2)
+    X3 = model.predict(input_img3)
+    concat = Concatenate()([X1, X2, X3])
+    X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(concat)
+    X = Convolution2D(filters=128, kernel_size=(7, 7), padding='same', activation='relu')(X)
+    X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(X)
+    X = Convolution2D(filters=64, kernel_size=(7, 7), padding='same', activation='relu')(X)
+    # X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(X)
+    # X = Convolution2D(filters=256, kernel_size=(7, 7), padding='same', activation='relu')(X)
+    X = Flatten()(X)
+    X = Dense(128, activation='relu')(X)
+    X = Dense(64, activation='relu')(X)
+    X = Dense(1, activation='sigmoid')(X)
+    final_model = Model(inputs=[input_img1, input_img2, input_img3], outputs=X)
+    return final_model
+
+
+finalModel = vgg16_feature_fusion()
+print(finalModel.summary())
