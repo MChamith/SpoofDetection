848b68f1283700b237ae431a2bf455ab909a5485
MChamith <mawelacm@gmail.com>
2020-01-21 12:29:27 +0530

    model
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- Spoof_vgg16.py
+++ Spoof_vgg16.py
@@ -4,25 +4,26 @@ from keras.models import Model
 import numpy as np
 from keras import Model
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Convolution2D, MaxPool2D, Input, Concatenate, Flatten
+from keras.layers import Dense, Activation, Convolution2D, MaxPool2D, Input, Concatenate, Flatten, concatenate
 import numpy as np
 import keras.backend as K
 from keras_applications.vgg19 import VGG19
 
 
 def vgg16_feature_fusion():
-    input_img1 = Input(shape=(224, 224, 3))  # channel first
-    input_img2 = Input(shape=(224, 224, 3))
-    input_img3 = Input(shape=(224, 224, 3))
+    base_model1 = VGG19(weights='imagenet', include_top=False)
+    base_model2 = VGG19(weights='imagenet', include_top=False)
+    base_model3 = VGG19(weights='imagenet', include_top=False)
 
-    base_model = VGG19(weights='imagenet')
-    model = Model(inputs=base_model.input, outputs=base_model.get_layer('block4_pool').output)
+    # model1 = Model(inputs=base_model1.input, outputs=base_model1.get_layer('block4_pool').output)
+    # model2 = Model(inputs=base_model2.input, outputs=base_model2.get_layer('block4_pool').output)
+    # model3 = Model(inputs=base_model3.input, outputs=base_model3.get_layer('block4_pool').output)
 
-    X1 = model.predict(input_img1)
-    X2 = model.predict(input_img2)
-    X3 = model.predict(input_img3)
-    concat = Concatenate()([X1, X2, X3])
-    X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(concat)
+    concatenated = concatenate(
+        [base_model1.get_layer('block4_pool').output, base_model2.get_layer('block4_pool').output,
+         base_model3.get_layer('block4_pool').output])
+
+    X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(concatenated)
     X = Convolution2D(filters=128, kernel_size=(7, 7), padding='same', activation='relu')(X)
     X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(X)
     X = Convolution2D(filters=64, kernel_size=(7, 7), padding='same', activation='relu')(X)
@@ -31,9 +32,13 @@ def vgg16_feature_fusion():
     X = Flatten()(X)
     X = Dense(128, activation='relu')(X)
     X = Dense(64, activation='relu')(X)
+    # X = Dense(128, activation='relu')(X)
+    # X = Dense(64, activation='relu')(X)
+    # print('before ' +str(X.shape))
     X = Dense(1, activation='sigmoid')(X)
-    final_model = Model(inputs=[input_img1, input_img2, input_img3], outputs=X)
-    return final_model
+    # print('after ' + str(X.shape))
+    model = Model(inputs=[base_model1.input, base_model2.input, base_model3.input], outputs=X)
+    return model
 
 
 finalModel = vgg16_feature_fusion()
