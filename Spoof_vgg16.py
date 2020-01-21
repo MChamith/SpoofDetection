c12d0ac18922b1477dbe9351423a1b28d5f71653
MChamith <mawelacm@gmail.com>
2020-01-21 13:25:30 +0530

    remove flatten
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- Spoof_vgg16.py
+++ Spoof_vgg16.py
@@ -29,7 +29,7 @@ def vgg16_feature_fusion():
     X = Convolution2D(filters=64, kernel_size=(7, 7), padding='same', activation='relu')(X)
     # X = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(X)
     # X = Convolution2D(filters=256, kernel_size=(7, 7), padding='same', activation='relu')(X)
-    X = Flatten()(X)
+    # X = Flatten()(X)
     X = Dense(128, activation='relu')(X)
     X = Dense(64, activation='relu')(X)
     # X = Dense(128, activation='relu')(X)
@@ -37,7 +37,7 @@ def vgg16_feature_fusion():
     # print('before ' +str(X.shape))
     X = Dense(1, activation='sigmoid')(X)
     # print('after ' + str(X.shape))
-    model = Model(inputs=[base_model1.input, base_model2.input, base_model3.input], outputs=X)
+    model = Model(inputs=[base_model1.input, base_model2.input, base_model3.input], outputs=X, in)
     return model
 
 
