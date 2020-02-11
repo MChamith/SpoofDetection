fd6782bc2b32e0f45febcf1f60d11176c0c76026
MChamith <mawelacm@gmail.com>
2020-02-11 16:24:19 +0530

    remove categorical
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- PatchDataGenerator.py
+++ PatchDataGenerator.py
@@ -119,5 +119,5 @@ class PatchDataGenerator(keras.utils.Sequence):
         # print('X_gray = ' + str(X_gray))
         # print('X_dog = ' + str(X_dog))
         # print('X_lbp = ' + str(X_lbp))
-        return [X_gray, X_dog, X_lbp], keras.utils.to_categorical(label, num_classes=self.n_classes)
+        return [X_gray, X_dog, X_lbp],label
         # return X, y
