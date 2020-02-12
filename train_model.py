2f37bf491153e16573a022a5ab526d7ff24316c7
MChamith <mawelacm@gmail.com>
2020-02-12 18:30:26 +0530

    changed numner of iterations to 32by number
    Signed-off-by: MChamith <mawelacm@gmail.com>

--- train_model.py
+++ train_model.py
@@ -90,5 +90,5 @@ model_history = model.fit_generator(generator=train_gen,
                                     callbacks=[check_pointer,
                                                reduce_lr, tensorboard_keras, early_stop, csv_logger],
                                     shuffle=True,
-                                    steps_per_epoch=1000, validation_steps=50
+                                    steps_per_epoch=1024, validation_steps=64
                                     )
